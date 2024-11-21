eb5f393dd12bb9402530893c57b220341717561d
b3codes <bhnpratap30@gmail.com>
2024-11-21 18:19:16 +0530

    Initial commit

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,27 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Star", {
+  enumerable: true,
+  get: function get() {
+    return _Star["default"];
+  }
+});
+Object.defineProperty(exports, "StarRatingProvider", {
+  enumerable: true,
+  get: function get() {
+    return _StarRatingContext.StarRatingProvider;
+  }
+});
+Object.defineProperty(exports, "useStarRating", {
+  enumerable: true,
+  get: function get() {
+    return _useStarRating.useStarRating;
+  }
+});
+var _Star = _interopRequireDefault(require("./components/Star"));
+var _StarRatingContext = require("./context/StarRatingContext");
+var _useStarRating = require("./hooks/useStarRating");
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
